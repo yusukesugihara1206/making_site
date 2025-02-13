e926717da2228850940d0088dd3ab12111123ad1
yusukesugihara1206 <sugisugi5548@gmail.com>
2025-02-13 21:57:37 +0900

    first commit

--- js/main.js
+++ js/main.js
@@ -1,24 +1,25 @@
-
-
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper(".swiper", {
   
+  pagination: {
+    el: ".swiper-pagination",
+    
+  },
   
-    direction: 'vertical',
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+
+  slidesPerView: 2,
+  
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  autoplay: {
+      delay: 5000,
+    },
+
     loop: true,
+   });
   
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
